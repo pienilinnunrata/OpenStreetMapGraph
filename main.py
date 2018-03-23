01a86b8cf7c6c7109b59cabf69adfed9416fe316
pienilinnunrata <pienilinnunrata@gmail.com>
2018-03-23 21:17:09 +0300

    Fix parsing
    Not all ways were parsed correctly and now the matrix is 267Mb which is A LOT, so the matrix will be removed. Also added all hospitals to ways

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 # ## Классы для работы с узлами и дорогами
 
-# In[40]:
+# In[1]:
 
 
 import svgwrite
@@ -33,70 +33,89 @@ class Node:
         self.is_in_road = False
         self.is_crossroad = False
         self.is_in_highway = False
+        self.is_in_hospital = False
         self.number_in_dict = 0
+        self.is_entrance = False
     def isDeletable(self):
         return not(self.is_end_node or self.is_start_node or self.is_crossroad )
 
 
 # ## Парсинг
 
-# In[29]:
+# In[133]:
 
 
 def parse_osm(filename = 'kal.osm'):
 
+    start_time = time.time()
+    
     print("Parsing .osm...")
-    counted_highways = {'motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified',
-                   'motorway_link', 'trunk_link',  'tertiary_link',
-                   'primary_link', 'secondary_link'} 
+    counted_highways = {'motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified', 'living_street',
+                   'motorway_link', 'trunk_link',  'tertiary_link', 'service',
+                   'primary_link', 'secondary_link', 'road'} 
     
     number_of_ways = 0
     number_of_nodes = 0
 
-    tree = etree.iterparse(filename, events=("start", "end"))
+    tree = etree.iterparse(filename, events=('start',"end",))
     for event, elem in tree:
         # parsing ways
-        if elem.tag == 'way' and event == 'start':
+        way = Way(0)
+        if elem.tag == 'way':
+            way.id = elem.get('id')
+            if event == 'end':
+                children = elem.getchildren()
+                nd = []
+                is_highway = False
+                is_hospital = False
 
-            way = Way(elem.get('id'))
-
-            children = elem.getchildren()
-            nd = []
-            is_highway = False
-
-            for child in children:
-                if child.tag == 'tag' and child.get('k') == 'highway' and child.get('v') in counted_highways :
-                    way.way_type = child.get('v')
-                    is_highway = True
-            if is_highway:
-                for child in children:
-                    if child.tag == 'nd':
-                        nd.append(child.get('ref'))
-                way.nodes = nd
-                ways.append(way)
-                number_of_ways += 1
-
+                for child in elem.iter('nd', 'tag'):
+                    if child.tag == 'tag' and child.get('k') == 'highway' and child.get('v') in counted_highways :
+                        way.way_type = child.get('v')
+                        is_highway = True
+                    if child.tag == 'tag' and child.get('k') == 'amenity' and child.get('v') == "hospital":
+                        way.way_type = 'hospital'
+                        is_hospital = True
+                if is_highway or is_hospital:
+                    for child in elem.iter('nd', 'tag'):
+                        if child.tag == 'nd':
+                            nd.append(child.get('ref'))
+                    way.nodes = nd
+                    ways.append(way)
+                    number_of_ways += 1
+                elem.clear()
         # parsing nodes
-        if elem.tag == 'node' and event == 'start':
+        if elem.tag == 'node':
             node = Node(elem.get('id'), elem.get('lat'), elem.get('lon'))
-            nodes[elem.get('id')] = node
-            number_of_nodes += 1
-        elem.clear()            
+            if event == "end":
+                children = elem.getchildren()
+                for child in children:
+                    if child.tag == 'tag' and child.get('k') == 'building' and child.get('v') == 'entrance':
+                        node.is_entrance = True
+                nodes[elem.get('id')] = node
+                number_of_nodes += 1 
+                elem.clear()
 
     print('Node number:',number_of_nodes)
     print('Road number:',number_of_ways)
+    time_final = (time.time() - start_time)
+    print("--- %s seconds ---" % time_final)
     print("Done")
 
 
 # ## Удаление промежуточных узлов
 
-# In[7]:
+# In[4]:
 
 
 def delete_transitional_nodes():
     print('\nDeleting transitional nodes...')
     print('Nodes number before:',len(nodes))
     for way in ways:
+        if way.way_type == 'hospital':
+            for node in way.nodes:
+                nodes.get(node).is_in_hospital = True
+                continue
         is_first_node = True
         nodenum = 0
         for node in way.nodes:
@@ -115,12 +134,14 @@ def delete_transitional_nodes():
 
     to_pop_list = []
     for node in nodes:
-        if not nodes.get(node).is_in_highway:
+        if not nodes.get(node).is_in_highway and not nodes.get(node).is_in_hospital:
             to_pop_list.append(str(node))
     for i in range(0,len(to_pop_list)-1):
         nodes.pop(to_pop_list[i])
 
     for way in ways:
+        if way.way_type == 'hospital':
+            continue
         list_to_remove = []
         for node in way.nodes:
             if nodes.get(node).isDeletable():
@@ -139,7 +160,7 @@ def delete_transitional_nodes():
 
 # ## Отрисовка карты 
 
-# In[13]:
+# In[129]:
 
 
 def trans(a):
@@ -147,7 +168,7 @@ def trans(a):
 def draw_map(svgurl = "pictures/kaliningrad_map.svg"): 
     print("\nDrawing map...")
     ratio = 1.3 # to avoid too much deformation
-    ypicsize = 3000
+    ypicsize = 5000
     xpicsize = int(ypicsize / ratio)
     ypicsizepx = str(xpicsize) + 'px'
     xpicsizepx = str(ypicsize) + 'px'
@@ -167,36 +188,45 @@ def draw_map(svgurl = "pictures/kaliningrad_map.svg"):
     svg_document.add(svg_document.text("Kaliningrad roadmap",insert = (40, 40)))
     svg_document.save()
 
-    black_roads = ['motorway', 'trunk', 'primary', 'secondary', 'tertiary']
-    green_roads = ['unclassified', 'residential']
+    yellow_roads = ['motorway','trunk','primary']
+    black_roads = ['secondary', 'tertiary','unclassified', 'residential']
+    hospitals = ['hospital']
     for elem in ways:
 
         color = 'blue' # small roads
         swidth = 1
+        if elem.way_type in yellow_roads:
+            color = 'yellow' # the biggest roads
+            swidth = 6
         if elem.way_type in black_roads:
             color = 'black' # big roads
             swidth = 3
-        if elem.way_type in green_roads:
-            color = 'green' # other roads
-            swidth = 2
+        if elem.way_type in hospitals:
+            color = 'red' # hospitals
+            swidth = 6
 
         points = []
         for i in range(0, len(elem.nodes)):
             y = (trans(maxlat) - trans(float(nodes.get(elem.nodes[i]).lat))) / scalelat
             x = ypicsize - (trans(maxlon) - trans(float(nodes.get(elem.nodes[i]).lon))) / scalelon
             points.append((x,y))
-            svg_document.add(svg_document.circle(center = (x,y), r = 2, stroke = "red")) # write nodes to svg
-        svg_document.add(svg_document.polyline(points, stroke=color, stroke_width = swidth, fill='none')) # write roads to svg
-
+    
+            point_color = 'grey'
+            point_r = 2
+            if nodes.get(elem.nodes[i]).is_entrance and nodes.get(elem.nodes[i]).is_in_hospital:
+                point_color = 'green'
+                point_r = 5
+            svg_document.add(svg_document.circle(center = (x,y), r = point_r, stroke = point_color, fill=point_color )) # write nodes to svg
+        svg_document.add(svg_document.polyline(points, stroke=color, stroke_width = swidth, fill='none')) # write roads to svg 
     time_final = (time.time() - start_time)
     print("--- %s seconds ---" % time_final)
     svg_document.save()
-    print("Done\n\n")
+    print("Done")
 
 
 # ## Матрица смежности и список смежности в .csv
 
-# In[9]:
+# In[135]:
 
 
 def write_csv(matrix_path = 'csv/adjacency_matrix.csv', list_path = 'csv/adjacency_list.csv'):
@@ -227,30 +257,22 @@ def write_csv(matrix_path = 'csv/adjacency_matrix.csv', list_path = 'csv/adjacen
 
     df_am.to_csv(matrix_path)
     df_al.to_csv(list_path)
-    print("Done\n\n")
+    print("Done")
+    
+    #df_am.head(10)
+    #df_al.head(10)
 
 
-# In[74]:
+# In[136]:
 
 
-df_am.head(10)
+if __name__ == "__main__":
+    ways = []
+    nodes = {}
 
-
-# In[75]:
-
-
-df_al.head(10)
-
-
-# In[43]:
-
-
-ways = []
-nodes = {}
-
-parse_osm()
-delete_transitional_nodes()
-draw_map()
-write_csv()
-print("All done")
+    parse_osm()
+    delete_transitional_nodes()
+    draw_map()
+    write_csv()
+    print("\nAll done")
 
